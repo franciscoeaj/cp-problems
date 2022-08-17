7756070b7c6dd806b77e4b76003b55bacf73cacb
franciscoeaj <francisco.edeverton@ccc.ufcg.edu.br>
2022-08-17 00:31:33 -0300

    Solution for problem 804

---
+++ LeetCode/804/804.py
@@ -0,0 +1,41 @@
+class Solution:
+def uniqueMorseRepresentations(self, words: List[str]) -> int:
+  alphabet = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--.."
+  }
+
+  unique_words = set()
+
+  for word in words:
+    morse_word = ""
+    for letter in word:
+      morse_word += alphabet[letter]
+
+    unique_words.add(morse_word)
+  
+  return len(unique_words)
